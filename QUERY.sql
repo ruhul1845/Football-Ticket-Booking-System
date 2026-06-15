1b391fcebd229402001a54dd4082257929e75f38
ruhul1845 <rohul4316@gmail.com>
2026-06-15 08:36:16 +0600

    query 7  added

--- QUERY.sql
+++ QUERY.sql
@@ -53,3 +53,8 @@ SELECT
     total_cost
 FROM bookings
 WHERE total_cost > (SELECT AVG(total_cost) FROM bookings);
+
+--Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match
+
+SELECT match_id ,fixture,base_ticket_price FROM matches
+ORDER BY base_ticket_price DESC LIMIT 2;
