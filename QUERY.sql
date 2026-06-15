e5b57ec9f58ef967b57442ad0fccdbf904eb4adf
ruhul1845 <rohul4316@gmail.com>
2026-06-15 08:28:33 +0600

    query 4 added

--- QUERY.sql
+++ QUERY.sql
@@ -25,3 +25,15 @@ FROM bookings
 WHERE payment_status IS NULL;
 
 --Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM bookings b
+INNER JOIN users u
+    ON b.user_id = u.user_id
+INNER JOIN matches m
+    ON b.match_id = m.match_id;
+
+--
