b8d01f85096179a002c2aa1779a06e6e4e453630
ruhul1845 <rohul4316@gmail.com>
2026-06-15 08:22:10 +0600

    query are added

---
+++ QUERY.sql
@@ -0,0 +1,27 @@
+--Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+SELECT * FROM matches
+
+WHERE tournament_category='Champions League' AND match_status='Available';
+
+--Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT
+    user_id,
+    full_name,
+    email
+FROM users
+WHERE full_name LIKE 'Tanvir%'
+   OR full_name LIKE '%Haque%';
+
+
+--Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') 
+FROM bookings
+WHERE payment_status IS NULL;
+
+--Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams
